adc3858935787881bb483c85963cb7fd61fc8008
ParthPrajapati2005 <109304629+ParthPrajapati2005@users.noreply.github.com>
2023-06-26 15:34:58 +0100

    project

--- index.js
+++ index.js
@@ -24,6 +24,11 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req,res) => {
+  const headers = req.headers;
+  res.json({ipaddress:headers["host"], language:headers["accept-language"], software:headers["user-agent"]});
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
